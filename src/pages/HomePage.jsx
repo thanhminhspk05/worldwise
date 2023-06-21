eb9af05f18ca0373ed0e3b0c32479e1cbe138bcd
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-06-21 14:36:38 +0700

    lesson 211

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
-import styles from "./Homepage.module.css";
+import PageNav from '../components/PageNav';
+import styles from './Homepage.module.css';
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav />
       <section>
         <h1>
           You travel the world.
@@ -10,9 +12,8 @@ export default function Homepage() {
           WorldWise keeps track of your adventures.
         </h1>
         <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          A world map that tracks your footsteps into every city you can think of. Never forget your wonderful
+          experiences, and show your friends how you have wandered the world.
         </h2>
       </section>
     </main>
