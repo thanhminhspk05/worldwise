df85a2e8823ec5a50496828cb59147611f02bb35
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-06-21 13:58:20 +0700

    lesson 211

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,8 @@
-import { Link } from 'react-router-dom';
-import PageNav from '../components/PageNav';
-import styles from './Homepage.module.css';
+import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      <PageNav />
-
       <section>
         <h1>
           You travel the world.
@@ -14,15 +10,10 @@ export default function Homepage() {
           WorldWise keeps track of your adventures.
         </h1>
         <h2>
-          A world map that tracks your footsteps into every city you can think of. Never forget your wonderful
-          experiences, and show your friends how you have wandered the world.
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
         </h2>
-        <Link
-          to="app"
-          className="cta"
-        >
-          Start tracking now
-        </Link>
       </section>
     </main>
   );
