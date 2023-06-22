77aec70cceb7eb7f24c1000b4b199ba4e3cb8f5c
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-06-22 09:34:13 +0700

    other reduce countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -10,12 +10,19 @@ function CountryList({ cities, isLoading }) {
 
   if (!cities.length) return <Message message="Add your first city by clicking on a city on the map" />;
 
-  const countries = cities.reduce((arr, city) => {
-    if (!arr.map((el) => el.country).includes(city.country))
-      return [...arr, { country: city.country, emoji: city.emoji }];
-    else return arr;
+  // const countries = cities.reduce((arr, city) => {
+  //   if (!arr.map((el) => el.country).includes(city.country))
+  //     return [...arr, { country: city.country, emoji: city.emoji }];
+  //   else return arr;
+  // }, []);
+
+  const countries = cities.reduce((acc, { country, emoji }) => {
+    if (!acc.some((item) => item.country === country)) {
+      return [...acc, { country, emoji }];
+    }
+    return acc;
   }, []);
-  console.log(countries);
+
   return (
     <ul className={styles.countryList}>
       {countries.map((country, index) => (
